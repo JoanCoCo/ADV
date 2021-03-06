206e320883e063f68c3cf5a614faee410330d668
Joan Colom Colom <joacoco@inf.upv.es>
2021-03-06 18:57:06 +0100

    Project2 progress

---
+++ Assets/Project 2/Scripts/TrapFlower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapFlower : MonoBehaviour
+{
+    private Animator _anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _anim.SetBool("Touched", true);
+    }
+}
