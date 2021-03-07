7f29a6726d9b8ceb5f563a0a824f53ea821f103a
Joan Colom Colom <joacoco@inf.upv.es>
2021-03-07 17:15:39 +0100

    Project 2

--- Assets/Project 2/Scripts/TrapFlower.cs
+++ Assets/Project 2/Scripts/TrapFlower.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class TrapFlower : MonoBehaviour
 {
     private Animator _anim;
+    public bool reset = false;
     // Start is called before the first frame update
     void Start()
     {
         _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(reset)
+        {
+            Debug.Log("Reset ON");
+            _anim.SetBool("Reset", true);
+            _anim.SetBool("Touched", false);
+        } else
+        {
+            Debug.Log("Reset OFF");
+            _anim.SetBool("Reset", false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
