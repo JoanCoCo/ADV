abdfad7832a0f82ea83bc3f972ed37915ec1d044
Joan Colom Colom <joacoco@inf.upv.es>
2021-03-25 18:48:39 +0100

    Project 3 progress.

---
+++ Assets/Project 3/Scripts/InteractionTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject TargetObject;
+    [SerializeField] private string VariableName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TargetObject.GetComponent<Animator>().SetBool(VariableName, true);
+    }
+}
