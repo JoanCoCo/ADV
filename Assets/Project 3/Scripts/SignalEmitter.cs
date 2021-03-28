68b7f433fe66e06e84b7fcf11e45db425cb90830
Joan Colom Colom <joacoco@inf.upv.es>
2021-03-28 19:08:36 +0200

    Progress in synchronization.

---
+++ Assets/Project 3/Scripts/SignalEmitter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalEmitter : MonoBehaviour
+{
+    [SerializeField] private GameObject[] target;
+    [SerializeField] private string[] keys;
+    private Animator[] targetAnimators;
+
+    public bool trigger = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAnimators = new Animator[target.Length];
+        for (int i = 0; i < target.Length; i++)
+        {
+            targetAnimators[i] = target[i].GetComponent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < target.Length; i++)
+        {
+            targetAnimators[i].SetBool(keys[i], trigger);
+        }
+    }
+}
