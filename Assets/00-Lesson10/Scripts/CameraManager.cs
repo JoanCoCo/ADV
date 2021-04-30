d06e1242324b483d9a1a4062de9e243f3da838df
Joan Colom Colom <joacoco@inf.upv.es>
2021-04-30 17:49:37 +0200

    Lesson 10 Nav Mesh

---
+++ Assets/00-Lesson10/Scripts/CameraManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private GameObject followingCamera;
+    [SerializeField] private GameObject controlledCamera;
+    [SerializeField] private KeyCode interactionKey = KeyCode.M;
+    private bool isInControl = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(interactionKey))
+        {
+            isInControl = !isInControl;
+            UpdateCamera();
+        }
+    }
+
+    private void UpdateCamera()
+    {
+        if(isInControl)
+        {
+            followingCamera.SetActive(false);
+            controlledCamera.SetActive(true);
+        } else
+        {
+            controlledCamera.SetActive(false);
+            followingCamera.SetActive(true);
+        }
+    }
+
+    public Camera GetActiveCamera()
+    {
+        return isInControl ? controlledCamera.GetComponent<Camera>() : followingCamera.GetComponent<Camera>();
+    }
+}
---
+++ Assets/00-Lesson10/Scripts/CoolCharacterNavAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class CoolCharacterNavAI : MonoBehaviour
+{
+    //public Transform goal;
+    private NavMeshAgent agent;
+    private Animator animator;
+    [SerializeField] private Transform destinationMark;
+    [SerializeField] private CameraManager cameraManager;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cameraManager.GetActiveCamera().ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.destination = destinationMark.position = hit.point;
+            }
+        }
+
+        if (agent.isOnOffMeshLink) animator.SetTrigger("Jump");
+        animator.SetFloat("XDir", transform.InverseTransformDirection(agent.velocity).x);
+        animator.SetFloat("YDir", transform.InverseTransformDirection(agent.velocity).z);
+    }
+}
