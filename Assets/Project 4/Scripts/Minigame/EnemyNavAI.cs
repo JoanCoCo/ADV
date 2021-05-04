aca5633b4e5a7b81c49479324cc70cc41c2a2872
Joan Colom Colom <joacoco@inf.upv.es>
2021-05-04 15:39:52 +0200

    Minigame preparation.

---
+++ Assets/Project 4/Scripts/Minigame/EnemyNavAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class EnemyNavAI : MonoBehaviour
+{
+    //public Transform goal;
+    private NavMeshAgent agent;
+    private Animator animator;
+    [SerializeField] private Transform destinationMark;
+    private enum State { On, Off };
+    [SerializeField] private PlayableDirector director;
+    [SerializeField] private State state = State.Off;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        agent.destination = destinationMark.position;
+    }
+
+    private void Update()
+    {
+        if (state == State.On && director.state != PlayState.Playing)
+        {
+            if (agent.isStopped) agent.isStopped = false;
+            if (agent.isOnOffMeshLink) animator.SetTrigger("Jump");
+            animator.SetFloat("XDir", transform.InverseTransformDirection(agent.velocity).x);
+            animator.SetFloat("YDir", transform.InverseTransformDirection(agent.velocity).z);
+        } else
+        {
+            if (!agent.isStopped) agent.isStopped = true;
+        }
+    }
+}
---
+++ Assets/Project 4/Scripts/Minigame/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController controller;
+    [SerializeField] private float gravity = 9.8f;
+    [SerializeField] private float jump = 2.0f;
+    private float xInput = 0;
+    private float yInput = 0;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        controller.Move(Vector3.down * gravity * Time.deltaTime);
+
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        animator.SetFloat("XDir", xInput);
+        animator.SetFloat("YDir", yInput);
+    }
+}
