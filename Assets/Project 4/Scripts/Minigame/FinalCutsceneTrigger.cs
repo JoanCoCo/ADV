a443c119c450c11fcf071a4a4eb77b227068145b
Joan Colom Colom <joacoco@inf.upv.es>
2021-05-04 20:49:22 +0200

    Alternate endings and player controller.

---
+++ Assets/Project 4/Scripts/Minigame/FinalCutsceneTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class FinalCutsceneTrigger : MonoBehaviour
+{
+    [SerializeField] private string enemyTag = "Enemy";
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private PlayableDirector badCutscene;
+    [SerializeField] private PlayableDirector goodCutscene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered by " + other.gameObject.tag);
+
+        if (other.gameObject.CompareTag(enemyTag))
+        {
+            badCutscene.Play();
+        } else if(other.gameObject.CompareTag(playerTag))
+        {
+            goodCutscene.Play();
+        }
+    }
+}
--- Assets/Project 4/Scripts/Minigame/PlayerController.cs
+++ Assets/Project 4/Scripts/Minigame/PlayerController.cs
@@ -10,8 +10,8 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     [SerializeField] private float gravity = 9.8f;
     [SerializeField] private float jump = 2.0f;
-    private float xInput = 0;
-    private float yInput = 0;
+    private float xInput = 0.0f;
+    private float yInput = 0.0f;
 
     private void Start()
     {
@@ -20,13 +20,26 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Update()
-    {
+    {   
+
         controller.Move(Vector3.down * gravity * Time.deltaTime);
-
-        xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
+        xInput = Input.GetAxis("Horizontal");
 
-        animator.SetFloat("XDir", xInput);
+        if (Mathf.Approximately(yInput, 0.0f))
+        {
+            animator.SetFloat("XDir", xInput);
+        }
+        else
+        {
+            transform.Rotate(Vector3.up, 90.0f * Time.deltaTime * xInput);
+            float xdir = animator.GetFloat("XDir");
+            if (Mathf.Abs(xdir) > 0.0f)
+            {
+                animator.SetFloat("XDir", xdir - Mathf.Sign(xdir) * 0.05f);
+            }
+        }
+
         animator.SetFloat("YDir", yInput);
     }
 }
